ee3997bcdc0c68012769de9f98b27443ea41c56c
LAPTOP-E6E5OM6U\vsprite <ex9405210@gmail.com>
2021-09-09 09:55:34 +0800

    create new table test

---
+++ database/migrations/2021_09_09_011805_create_member_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name');
+            $table->timestamps('signin');
+            $table->timestamps('signout');
+            $table->timestamps('dutydate');
+            $table->timestamps('lastdate');
+            $table->text('remark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+}
