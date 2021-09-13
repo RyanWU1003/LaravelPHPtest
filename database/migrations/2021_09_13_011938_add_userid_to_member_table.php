eb5a7845156f6219fec4605da6cd64f3b9418b4f
LAPTOP-E6E5OM6U\vsprite <ex9405210@gmail.com>
2021-09-13 09:51:54 +0800

    new Colum

--- database/migrations/2021_09_13_011938_add_userid_to_member_table.php
+++ database/migrations/2021_09_13_011938_add_userid_to_member_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMemberTable extends Migration
+class AddUseridToMemberTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateMemberTable extends Migration
      */
     public function up()
     {
-        Schema::create('member', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamp('signin');
-            $table->timestamp('signout');
-            $table->text('remark');
-            $table->timestamps();
+        Schema::table('member', function (Blueprint $table) {
+            //
+            $table->string('userid');
         });
     }
 
@@ -30,6 +26,9 @@ class CreateMemberTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('member');
+        Schema::table('member', function (Blueprint $table) {
+            //
+            $table->dropColumn('userid');
+        });
     }
 }
